1245ae4c24bd8c4a9f5817cabcb95bb03fdedcc8
Ishtmeet Singh <ish.rissam@gmail.com>
2018-04-01 01:27:15 +0530

    bullet now gets removed from the array instead of hiding

--- Bullet.js
+++ Bullet.js
@@ -2,10 +2,18 @@ var Bullet = function(_x, _radius) {
     this.xPos = _x;
     this.yPos = height - 70;
     this.radius = 10;
+    this.isHit = false;
 
     this.draw = function() {
         fill(106, 0, 173);
         ellipse(this.xPos, this.yPos, this.radius);
+
+        for(var index = 0; index < alienArray.length; index++) {
+            if(dist(this.xPos, this.yPos, alienArray[index].xPos, alienArray[index].yPos) < this.radius + alienArray[index].radius) {
+                this.isHit = true;
+                alienArray[index].health -= 25;
+            }
+        }
     }
     this.move = function() {
         this.yPos -= 15;
