312a83d5ed414d1788d92c35082df456ae37bc9a
Ishtmeet Singh <ish.rissam@gmail.com>
2018-04-01 01:26:40 +0530

    added colors to specify the health levels

---
+++ Aliens.js
@@ -0,0 +1,31 @@
+var Alien = function(_x) {
+    this.xPos = _x;
+    this.yPos = 50;
+    this.health = 100;
+    this.radius = 20;
+    this.isDead = false;
+
+    this.draw = function() {
+        if(this.health === 100) {
+            fill('green');
+        } else if(this.health === 75) {
+            fill('yellow');
+        } else if(this.health === 50) {
+            fill('orange');
+        } else if(this.health === 25) {
+            fill('red')
+        } else {
+            fill('black')
+        }
+        
+        ellipse(this.xPos, this.yPos, this.radius * 2, this.radius * 2);
+        this.yPos += 0.5;
+        if(this.health <= 0) {
+            this.isDead = true;
+        }
+    }
+
+    this.gotHit = function() {
+        this.health -= 25;
+    }
+}
