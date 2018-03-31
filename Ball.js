61c6ef0e2b4e51559716d4f7ee4d3fa18a9ed3cc
Ishtmeet Singh <ish.rissam@gmail.com>
2018-04-01 00:07:58 +0530

    init files

---
+++ Ball.js
@@ -0,0 +1,53 @@
+function calculateXPosition(_pos, _radius) {
+    if (_pos < _radius) {
+        return _pos + _radius;
+    } else if (_pos > width - _radius) {
+        return width - _pos;
+    } else return _pos
+}
+
+function calculateYPosition(_pos, _radius) {
+    if (_pos < _radius) {
+        return _pos + _radius;
+    } else if (_pos > height - _radius) {
+        return height - _pos;
+    } else return _pos
+}
+
+var Ball = function(_xPos, _yPos, _radius, _speedX, _speedY, _colour) {
+    var xValue =
+        this.xPos = calculateXPosition(_xPos, _radius);
+    this.yPos = calculateYPosition(_yPos, _radius);
+    this.radius = _radius;
+    this.speedX = floor(_speedX) === 0 ? _speedX + 1 : _speedX;
+    this.speedY = floor(_speedY) === 0 ? _speedY + 1 : _speedY;
+    this.colour = _colour;
+
+    this.drawBall = function() {
+        fill(this.colour);
+        ellipse(this.xPos + this.radius, this.yPos + this.radius, this.radius * 2, this.radius * 2);
+    }
+
+    this.update = function() {
+        this.drawBall();
+        if (this.xPos < 0 || this.xPos > width - this.radius) {
+            this.speedX *= -1;
+        }
+
+        if (this.yPos < 0 || this.yPos > height - this.radius) {
+            this.speedY *= -1;
+        }
+
+        this.xPos += this.speedX;
+        this.yPos += this.speedY;
+    }
+
+    this.checkCollision = function(_ball) {
+        if (dist(this.xPos, this.yPos, _ball.xPos, _ball.yPos) < this.radius + _ball.radius) {
+            this.speedX *= -1;
+            _ball.speedX *= -1;
+            this.speedY *= -1;
+            _ball.speedY *= -1;
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,53 @@
+var ballContainer = [];
+
+function randomXYGenerator(_limit) {
+    return random(20, _limit);
+}
+
+function randomRadiusGenerator() {
+    return random(10, 30);
+}
+
+function randomSpeedGenerator() {
+    var speed = random(-1, 1);
+    return floor(speed) == 0 ? 1 : speed;
+}
+
+function randomColorGenerator() {
+    return color(random(0, 255), random(0, 255), random(0, 255));
+}
+
+function drawBalls() {
+    for (var index = 0; index < ballContainer.length; index++) {
+        ballContainer[index].update();
+    }
+}
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    for (var index = 0; index < 7; index++) {
+        ballContainer.push(
+            new Ball(
+                randomXYGenerator(width),
+                randomXYGenerator(height),
+                randomRadiusGenerator(),
+                randomSpeedGenerator(),
+                randomSpeedGenerator(),
+                randomColorGenerator()
+            )
+        );
+    }
+}
+
+var count = 0;
+
+function draw() {
+    background(123)
+    for (var index = 0; index < ballContainer.length - 1; index++) {
+        for (var inner = index + 1; inner < ballContainer.length; inner++) {
+            ballContainer[index].checkCollision(ballContainer[inner])
+        }
+    }
+    drawBalls();
+
+}
